366508ef56c3b65fc2db72de48f7139fdaeb3c0a
luizcsdev <luizcsdev@gmail.com>
2025-03-19 20:25:02 -0300

    Criado de metodo para povoar a tabela Cidades

---
+++ database/factories/CidadeFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cidade>
+ */
+class CidadeFactory extends Factory
+{
+    protected $model = \App\Models\Cidade::class;
+
+    public function definition(): array
+    {
+        return [
+            'cid_nome' => $this->faker->city(),
+            'cid_uf' => $this->faker->stateAbbr(),
+        ];
+    }
+}
